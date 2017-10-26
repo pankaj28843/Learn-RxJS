1b476fd30758d2c766ca86f224493b5780a12898
Pankaj Kumar Singh <pankaj28843@gmail.com>
2017-10-26 13:31:00 +0200

    add retry startegy using Observable

--- main.ts
+++ main.ts
@@ -1,24 +1,54 @@
 import {Observable} from "rxjs";
 
-let source = Observable.fromEvent(document, "mousemove")
-    .map((e: MouseEvent) => {
-        return {
-            x: e.clientX,
-            y: e.clientY
-        }
-    })
-    .filter(value => value.x < 500)
-    .delay(300);
+let output = document.getElementById('output');
+let button = document.getElementById('button');
 
-function onNext(value){
-    let circle = document.getElementById('circle');
+let click = Observable.fromEvent(button, "click");
 
-    circle.style.left = `${value.x}px`;
-    circle.style.top = `${value.y}px`;
+
+function load(url: string) {
+    return Observable.create(observer => {
+        let xhr = new XMLHttpRequest();
+
+        xhr.addEventListener("load", () => {
+            if (xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText);
+                observer.next(data);
+                observer.complete();
+            } else {
+                observer.error(xhr.statusText);
+            }
+        });
+        xhr.open("GET", url);
+        xhr.send();
+    }).retryWhen(retryStrategy({attempts: 3, delay: 1500}));
 }
 
-source.subscribe(
-    onNext,
-    error => console.log(`error: ${error}`),
-    () => console.log(`complete`)
-);
+
+function retryStrategy({attempts = 4, delay = 1000}) {
+    return function (errors) {
+        return errors
+            .scan((acc, value) => {
+            console.log(acc, value);
+                return acc + 1
+            }, 0)
+            .takeWhile(acc => acc < attempts)
+            .delay(delay);
+    };
+}
+
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement("div");
+        div.innerText = m.title;
+        output.appendChild(div);
+
+    });
+}
+
+click.flatMap(e => load("movies.json"))
+    .subscribe(
+        renderMovies,
+        error => console.log(`error: ${error}`),
+        () => console.log(`complete`)
+    );
